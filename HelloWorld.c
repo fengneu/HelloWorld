e3c2466c9f569f443bf520b198da3889e62b7325
openfly <yuanfengbyd@hotmail.com>
2016-03-22 14:02:59 +0800

    Create HelloWorld.c

---
+++ HelloWorld.c
@@ -0,0 +1,13 @@
+#########################
+#
+# Hello World
+#
+#########################
+#include <stdio.h>
+
+int main(void)
+{
+        printf("hello, openwrt!\n");
+        printf("hello, world!\n");
+        return 0;
+}
